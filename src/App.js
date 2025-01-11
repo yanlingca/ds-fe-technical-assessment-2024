2804e0f794f3a2a78e76e602b730106fbc34cd99
yanlingca <129186479+yanlingca@users.noreply.github.com>
2025-01-11 21:27:32 +0800

    remove scroll offset

--- src/App.js
+++ src/App.js
@@ -26,35 +26,6 @@ function App() {
 
     return isMobile;
   };
-
-  useEffect(() => {
-    // Add offset to advoid fixed navbar cover the contents
-    const handleScroll = (e) => {
-      if (e.target.hash) {
-        e.preventDefault();
-        const target = document.querySelector(e.target.hash);
-        const navbarHeight = document.querySelector('.navbar').offsetHeight;
-        const scrollPosition = target.offsetTop - navbarHeight;
-
-        window.scrollTo({
-          top: scrollPosition,
-          behavior: 'smooth'
-        });
-      }
-    };
-
-    const links = document.querySelectorAll('a[href^="#"]');
-    links.forEach(link => {
-      link.addEventListener('click', handleScroll);
-    });
-
-    // Remove the event listener
-    return () => {
-      links.forEach(link => {
-        link.removeEventListener('click', handleScroll);
-      });
-    };
-  }, []);
   
   return (
     <div>
