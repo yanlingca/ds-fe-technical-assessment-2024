ae9f37922fe9d5cbfbd0ea5bdad5cc9764df6fb7
yanlingca <129186479+yanlingca@users.noreply.github.com>
2025-01-12 14:38:18 +0800

    collapse menu after clicking nav items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; //
 import LogoImageFile from '../assets/img/company-logo.png'
 import '../assets/style/header.css'
 
 function Header({isMobile}) {
+
+
+  const [expanded, setExpanded] = useState(false);
+  const handleToggle = () => {
+    setExpanded(!expanded);
+  };
+  const collapseNavbar = () => {
+    setExpanded(false); // Collapse navbar after click nav items
+  };
+
   return (
     <nav id="header" className="navbar navbar-expand-sm navbar-light bg-white custom-nav p-3">
       <div className="container-fluid">
@@ -12,13 +22,16 @@ function Header({isMobile}) {
         <a className="navbar-brand" href="#header"><img src={LogoImageFile} /></a>
         
         {/* Navbar Collapse Section */}
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div
+          className={`collapse navbar-collapse ${expanded ? 'show' : ''}`}
+          id="navbarNav"
+        >
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link" href="#feature">Feature</a>
+              <a className="nav-link" href="#feature" onClick={collapseNavbar}>Feature</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#project">Projects</a>
+              <a className="nav-link" href="#project" onClick={collapseNavbar}>Projects</a>
             </li>
           </ul>
         </div>
@@ -36,7 +49,8 @@ function Header({isMobile}) {
                 data-bs-target="#navbarNav"
                 aria-controls="navbarNav"
                 aria-expanded="false"
-                aria-label="Toggle navigation">
+                aria-label="Toggle navigation"
+                onClick={handleToggle}>
           <i className="bi bi-list fs-1"></i>
         </button>
       </div>
