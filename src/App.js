2520b90e69165ce939fbad8ce82529af64a6fea6
yanlingca <129186479+yanlingca@users.noreply.github.com>
2025-01-12 14:19:34 +0800

    add backToTop button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './assets/style/App.css';
 import './assets/style/animation.css';
 import './assets/style/custom-bootstrap.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Banner from './components/Banner';
@@ -26,6 +27,45 @@ function App() {
 
     return isMobile;
   };
+
+  const BackToTopButton = () => {
+    const [showButton, setShowButton] = useState(false);
+  
+    useEffect(() => {
+      const handleScroll = () => {
+        if (window.scrollY > 300) {
+          setShowButton(true);
+        } else {
+          setShowButton(false);
+        }
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+  
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      });
+    };
+  
+    return (
+      <>
+        {showButton && (
+          <button
+            onClick={scrollToTop}
+            className="position-fixed back-to-top-btn bottom-0 end-0 m-2">
+            <i className="bi bi-arrow-up-circle-fill" style={{flexShrink: 0 }}></i>
+          </button>
+        )}
+      </>
+    );
+  };
   
   return (
     <div>
@@ -36,6 +76,7 @@ function App() {
       <ProjectList isMobile={IsMobileCheck()} />
       <RegForm />
       <Footer isMobile={IsMobileCheck()} />
+      <BackToTopButton />
     </div>
   );
 }
